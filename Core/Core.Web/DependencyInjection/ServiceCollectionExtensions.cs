fa354d51a62fb1fb847b5a3763fffa7f636195ce
rrymar <rrymar@outlook.com>
2020-11-16 11:48:59 +0200

    core modules

---
+++ Core/Core.Web/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Core.App;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Web.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterModule<T>(this IServiceCollection services)
+            where T : IModule, new()
+        {
+            var module = new T();
+            module.Register(services);
+        }
+
+        public static void RegisterTopLevelModule<T>(this IServiceCollection services,
+            IMvcBuilder mvcBuider, IConfiguration configuration)
+            where T : ITopLevelModule, new()
+        {
+            var module = new T();
+            mvcBuider.AddApplicationPart(module.GetType().Assembly);
+
+            module.Register(services, configuration);
+        }
+    }
+}
