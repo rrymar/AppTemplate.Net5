59d26e00ae16301faab1641a5288679f6f9fe444
rrymar <rrymar@outlook.com>
2020-12-31 10:51:19 +0200

    navbar

--- App/AppTemplate.App.Web.React/ClientApp/src/App.tsx
+++ App/AppTemplate.App.Web.React/ClientApp/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {AppBar, Button, makeStyles, Toolbar, Typography} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    navigateButton: {
+        marginLeft: theme.spacing(2),
+    },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        App Template
+                    </Typography>
+                    <Button color="inherit" className={classes.navigateButton}>Users</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
 
 export default App;
