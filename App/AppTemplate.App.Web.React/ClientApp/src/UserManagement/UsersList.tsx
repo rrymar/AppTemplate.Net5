9469bc3a8c9ae9c61e7d24b728311e2022779b56
rrymar <rrymar@outlook.com>
2020-12-31 14:28:14 +0200

    grid

--- App/AppTemplate.App.Web.React/ClientApp/src/UserManagement/UsersList.tsx
+++ App/AppTemplate.App.Web.React/ClientApp/src/UserManagement/UsersList.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {Columns, DataGrid, RowsProp} from '@material-ui/data-grid';
+import {Columns, DataGrid, PageChangeParams, SortItem, SortModel, SortModelParams} from '@material-ui/data-grid';
 import {Typography} from '@material-ui/core';
 import {User} from './user';
 import {SearchQuery} from '../Core/searchQuery';
@@ -14,34 +14,67 @@ const columns: Columns = [
     {field: 'email', headerName: 'Email', width: 130},
 ];
 
-const rows: RowsProp = [
-    {id: 1, username: 'Snow', fullName: 'Jon Vissel', createdOn: new Date(), email: 'rr@rr.com'},
-    {id: 2, username: 'Lannister', fullName: 'Cersei Iron', createdOn: new Date(), email: null},
+const rows: User[] = [
+    {
+        id: 1, username: 'Snow', fullName: 'Jon Vissel',
+        createdOn: new Date(), email: 'rr@rr.com', firstName: 'aa', lastName: 'sdd', isSystemUser: false
+    },
 ];
 
 function UsersList() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState<User[]>([]);
+    const [page, setPage] = useState(1);
+    const [pageSize] = useState(5);
+    const [totalCount, setTotalCount] = useState<number>(0);
+
+    const [sortModel, setSortModel] = useState<SortModel>(
+        [{field: 'id', sort: 'asc'}]
+    );
+
+    const handlePageChange = (params: PageChangeParams) => {
+        setPage(params.page);
+    };
+
+    const handleSortModelChange = (params: SortModelParams) => {
+        setSortModel(params.sortModel);
+    };
 
     useEffect(() => {
-        let query = new SearchQuery();
+        let query: SearchQuery = {
+            pageSize: pageSize,
+            pageIndex: page - 1,
+            sortField: sortModel[0]?.field,
+            isDesc: sortModel[0]?.sort === 'desc',
+            keyword: ''
+        };
         axios.post<ResultsList<User>>('api/Users/Search', query)
             .then(res => {
-                setIsLoaded(true);
                 setItems(res.data.items);
+                setTotalCount(res.data.totalCount);
+                setIsLoaded(true);
             });
-    },[]);
+    }, [page, sortModel, pageSize]);
 
     return (
         <div>
             <Typography variant="h6">
                 Users
             </Typography>
-            { isLoaded &&
-                <div style={{height: 400, width: '100%'}}>
-                    <DataGrid rows={items} columns={columns} pageSize={10}/>
-                </div>
-            }
+            <div style={{height: 400, width: '100%'}}>
+                <DataGrid
+                    key={'id'}
+                    rows={items}
+                    columns={columns}
+                    loading={!isLoaded}
+                    pageSize={pageSize}
+                    paginationMode={'server'}
+                    rowCount={totalCount}
+                    onPageChange={handlePageChange}
+                    sortingMode={'server'}
+                    onSortModelChange={handleSortModelChange}
+                />
+            </div>
         </div>
     );
 }
